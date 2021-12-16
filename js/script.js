ca48d34cd1f1ac5706dea487bda4a8ee25134b2e
ChriEffe <christianferrazzano93@gmail.com>
2021-12-16 13:22:45 +0100

    inserisco dati

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 /*Partendo dal markup allegato, rifare lo slider ma questa volta usando Vue.*/
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            images: [
+                'image1.jpg',
+                'image2.jpg',
+                'image3.jpg',
+                'image4.jpg'
+            ]
+        }
+    },
+    methods: {
+        prev: function () {
+            
+        },
+        next: function () {
+            
+        }
+    }
+)
